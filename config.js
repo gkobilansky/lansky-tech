fb081bf90e0fcdf5a227c239c8a2c1483537cbe7
Gene <gene@lancekey.com>
2024-10-29 12:36:22 -0400

    update some configs

--- config.js
+++ config.js
@@ -49,13 +49,13 @@ const config = {
     // subdomain to use when sending emails, if you don't have a subdomain, just remove it. Highly recommended to have one (i.e. mg.yourdomain.com or mail.yourdomain.com)
     subdomain: "mg",
     // REQUIRED — Email 'From' field to be used when sending magic login links
-    fromNoReply: `Lancekey <noreply@lancekey.com>`,
+    fromNoReply: `Lansky <noreply@lansky.tech>`,
     // REQUIRED — Email 'From' field to be used when sending other emails, like abandoned carts, updates etc..
-    fromAdmin: `Gene at Lancekey <gene@lancekey.com>`,
+    fromAdmin: `Gene at Lansky <gene@lansky.tech>`,
     // Email shown to customer if need support. Leave empty if not needed => if empty, set up Crisp above, otherwise you won't be able to offer customer support."
-    supportEmail: "hi@lancekey.com",
+    supportEmail: "hi@lansky.tech",
     // When someone replies to supportEmail sent by the app, forward it to the email below (otherwise it's lost). If you set supportEmail to empty, this will be ignored.
-    forwardRepliesTo: "hi@lancekey.com",
+    forwardRepliesTo: "hi@lansky.tech",
   },
   colors: {
     // REQUIRED — The DaisyUI theme to use (added to the main layout.js). Leave blank for default (light & dark mode). If you any other theme than light/dark, you need to add it in config.tailwind.js in daisyui.themes.
