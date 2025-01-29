6628ca829b488ab86032c2964c68800996a709c8
Gene <gene@lancekey.com>
2025-01-29 10:50:22 -0500

    remove autoplay from slider, change accent color

--- app/work/_assets/components/MuxVideoSlider.js
+++ app/work/_assets/components/MuxVideoSlider.js
@@ -3,7 +3,7 @@
 import { useState, useEffect, useRef } from "react";
 import MuxVideoPlayer from "./MuxVideoPlayer";
 
-export default function MuxVideoSlider({ videos, autoPlay = "muted", theme = false }) {
+export default function MuxVideoSlider({ videos }) {
     const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
     const [progress, setProgress] = useState(0);
     const videoRef = useRef(null);
@@ -27,8 +27,8 @@ export default function MuxVideoSlider({ videos, autoPlay = "muted", theme = fal
                 <MuxVideoPlayer
                     playbackId={videos[currentVideoIndex].playbackId}
                     metadata={videos[currentVideoIndex].metadata}
-                    autoPlay={autoPlay}
-                    theme={theme}
+                    autoPlay={false}
+                    theme={false}
                     controls={false}
                     loop={false}
                     onEnded={handleEnded}
